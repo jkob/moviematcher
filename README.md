049dbc9612feee38aba99d8b3a199f712199488d
JakobLofgren <jakob.lofgren1@gmail.com>
2017-01-29 15:08:50 +0100

    Moved commented fake data, added installation instructions

--- README.md
+++ README.md
@@ -8,5 +8,13 @@ Tools used:
 [TheMovieDB]()
 
 
+## How to use:
+
+`git clone https://github.com/JakobLofgren/moviematcher && cd moviematcher`
+`npm i`
+create config.js at root level (moviematcher/) with your MovieDB API key.
+`npm start`
+
+
 Created by JakobLofgren 2017.
 
--- src/Matcher.jsx
+++ src/Matcher.jsx
@@ -8,71 +8,6 @@ import MatcherMovies from "./MatcherMovies.jsx"
 
 const API_URL = "https://api.themoviedb.org/3/discover/movie"
 import key from "../config.js"
-/*
-const fake_results = [{
-      "poster_path": "/kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
-      "adult": false,
-      "overview": "An apocalyptic story set in the furthest reaches of our planet, in a stark desert landscape where humanity is broken, and most everyone is crazed fighting for the necessities of life. Within this world exist two rebels on the run who just might be able to restore order. There's Max, a man of action and a man of few words, who seeks peace of mind following the loss of his wife and child in the aftermath of the chaos. And Furiosa, a woman of action and a woman who believes her path to survival may be achieved if she can make it across the desert back to her childhood homeland.",
-      "release_date": "2015-05-13",
-      "genre_ids": [
-        28,
-        12,
-        878,
-        53
-      ],
-      "id": 76341,
-      "original_title": "Mad Max: Fury Road",
-      "original_language": "en",
-      "title": "Mad Max: Fury Road",
-      "backdrop_path": "/tbhdm8UJAb4ViCTsulYFL3lxMCd.jpg",
-      "popularity": 38.859482,
-      "vote_count": 6374,
-      "video": false,
-      "vote_average": 7.1
-    },
-    {
-      "poster_path": "/qjiskwlV1qQzRCjpV0cL9pEMF9a.jpg",
-      "adult": false,
-      "overview": "A rogue band of resistance fighters unite for a mission to steal the Death Star plans and bring a new hope to the galaxy.",
-      "release_date": "2016-12-14",
-      "genre_ids": [
-        28,
-        12,
-        14,
-        878
-      ],
-      "id": 330459,
-      "original_title": "Rogue One: A Star Wars Story",
-      "original_language": "en",
-      "title": "Rogue One: A Star Wars Story",
-      "backdrop_path": "/tZjVVIYXACV4IIIhXeIM59ytqwS.jpg",
-      "popularity": 28.730003,
-      "vote_count": 1911,
-      "video": false,
-      "vote_average": 7.2
-    },
-    {
-      "poster_path": "/inVq3FRqcYIRl2la8iZikYYxFNR.jpg",
-      "adult": false,
-      "overview": "Based upon Marvel Comics’ most unconventional anti-hero, DEADPOOL tells the origin story of former Special Forces operative turned mercenary Wade Wilson, who after being subjected to a rogue experiment that leaves him with accelerated healing powers, adopts the alter ego Deadpool. Armed with his new abilities and a dark, twisted sense of humor, Deadpool hunts down the man who nearly destroyed his life.",
-      "release_date": "2016-02-09",
-      "genre_ids": [
-        28,
-        12,
-        35,
-        10749
-      ],
-      "id": 293660,
-      "original_title": "Deadpool",
-      "original_language": "en",
-      "title": "Deadpool",
-      "backdrop_path": "/nbIrDhOtUpdD9HKDBRy02a8VhpV.jpg",
-      "popularity": 13.843559,
-      "vote_count": 6299,
-      "video": false,
-      "vote_average": 7.2
-    }]
-*/
 
 
 export default class Matcher extends Component {
@@ -177,3 +112,70 @@ export default class Matcher extends Component {
 		)
 	}
 }
+
+
+/*
+const fake_results = [{
+      "poster_path": "/kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
+      "adult": false,
+      "overview": "An apocalyptic story set in the furthest reaches of our planet, in a stark desert landscape where humanity is broken, and most everyone is crazed fighting for the necessities of life. Within this world exist two rebels on the run who just might be able to restore order. There's Max, a man of action and a man of few words, who seeks peace of mind following the loss of his wife and child in the aftermath of the chaos. And Furiosa, a woman of action and a woman who believes her path to survival may be achieved if she can make it across the desert back to her childhood homeland.",
+      "release_date": "2015-05-13",
+      "genre_ids": [
+        28,
+        12,
+        878,
+        53
+      ],
+      "id": 76341,
+      "original_title": "Mad Max: Fury Road",
+      "original_language": "en",
+      "title": "Mad Max: Fury Road",
+      "backdrop_path": "/tbhdm8UJAb4ViCTsulYFL3lxMCd.jpg",
+      "popularity": 38.859482,
+      "vote_count": 6374,
+      "video": false,
+      "vote_average": 7.1
+    },
+    {
+      "poster_path": "/qjiskwlV1qQzRCjpV0cL9pEMF9a.jpg",
+      "adult": false,
+      "overview": "A rogue band of resistance fighters unite for a mission to steal the Death Star plans and bring a new hope to the galaxy.",
+      "release_date": "2016-12-14",
+      "genre_ids": [
+        28,
+        12,
+        14,
+        878
+      ],
+      "id": 330459,
+      "original_title": "Rogue One: A Star Wars Story",
+      "original_language": "en",
+      "title": "Rogue One: A Star Wars Story",
+      "backdrop_path": "/tZjVVIYXACV4IIIhXeIM59ytqwS.jpg",
+      "popularity": 28.730003,
+      "vote_count": 1911,
+      "video": false,
+      "vote_average": 7.2
+    },
+    {
+      "poster_path": "/inVq3FRqcYIRl2la8iZikYYxFNR.jpg",
+      "adult": false,
+      "overview": "Based upon Marvel Comics’ most unconventional anti-hero, DEADPOOL tells the origin story of former Special Forces operative turned mercenary Wade Wilson, who after being subjected to a rogue experiment that leaves him with accelerated healing powers, adopts the alter ego Deadpool. Armed with his new abilities and a dark, twisted sense of humor, Deadpool hunts down the man who nearly destroyed his life.",
+      "release_date": "2016-02-09",
+      "genre_ids": [
+        28,
+        12,
+        35,
+        10749
+      ],
+      "id": 293660,
+      "original_title": "Deadpool",
+      "original_language": "en",
+      "title": "Deadpool",
+      "backdrop_path": "/nbIrDhOtUpdD9HKDBRy02a8VhpV.jpg",
+      "popularity": 13.843559,
+      "vote_count": 6299,
+      "video": false,
+      "vote_average": 7.2
+    }]
+*/
