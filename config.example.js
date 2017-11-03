b0e4bbe58ece58cf4fcdbee62566139b15df3b2f
jkob <jakob.lofgren1@gmail.com>
2017-11-03 14:22:12 +0100

    add config example

---
+++ config.example.js
@@ -0,0 +1,7 @@
+/*
+	Steps to get an API key
+	Sign up to https://www.themoviedb.org
+	And follow these instructions https://developers.themoviedb.org/3/getting-started
+*/
+
+module.export = "YOUR_API_KEY"
