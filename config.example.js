b09a436f1c1290b5c62802f6f3a501b601aca9c4
jkob <jakob.lofgren1@gmail.com>
2017-11-03 14:36:24 +0100

    updated config.example.js

--- config.example.js
+++ config.example.js
@@ -1,7 +1,9 @@
 /*
-	Steps to get an API key
+	Create config.js in the same directory as this file
 	Sign up to https://www.themoviedb.org
-	And follow these instructions https://developers.themoviedb.org/3/getting-started
+	And follow these instructions to get your key https://developers.themoviedb.org/3/getting-started
+	Replace "YOUR_API_KEY" with the key you got from https://developers.themoviedb.org/3/getting-started
+	And paste that code into config.js
 */
 
 module.export = "YOUR_API_KEY"
